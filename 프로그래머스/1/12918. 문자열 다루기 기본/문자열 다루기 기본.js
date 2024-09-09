1d63cbd7d28693beada22b568160985bac15f442
HYERIN LEE <77167694+HYERINI@users.noreply.github.com>
2024-09-09 22:07:06 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,6 @@
+const solution = (s) => {
+    return ((s.length == 4 || s.length == 6) && isNaN(Number(s)) ? false : true)
+    // console.log(Number(s))
+    // console.log(isNan(2));
+    // console.log(isNan(a));
+}
