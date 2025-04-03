2ca607d52982a5b750d3ae543737123957f57429
HYERIN LEE <77167694+HYERINI@users.noreply.github.com>
2025-04-04 02:29:40 +0900

    [level 0] Title: 문자열 돌리기, Time: 188.53 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        for(char s : a.toCharArray()){
+            System.out.println(s);
+        }
+    }
+}
